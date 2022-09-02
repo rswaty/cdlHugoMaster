50ed61edce04f0f235e1ce3044351bc203bf6ec7
Randy Swaty <rswaty@gmail.com>
2022-09-02 10:34:05 -0400

    added Rhayna's map to carousel

--- carousel/carousel.R
+++ carousel/carousel.R
@@ -7,7 +7,8 @@ library(slickR)
 carouselImages <- c("carousel/1.png", 
               "carousel/2.png", 
               "carousel/3.png", 
-              "carousel/4.png")
+              "carousel/4.png",
+              "carousel/5.png")
 carousel <- slickR(obj = carouselImages, height=550, width='100%') 
 
 
